cb89e0c1b0be456ec1df5ee0af0e3097eb6cc895
zonin <business.kariukimartin@gmail.com>
2023-06-09 08:55:02 +0300

    data to render

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import CurrentWeather from './src/components/CurrentWeather';
+import UpComingWeather from './src/components/UpComingWeather';
 const App = () => {
   return (
     <View style={styles.container}>
-      <CurrentWeather />
+      {/* <CurrentWeather /> */}
+      <UpComingWeather />
     </View>
   );
 };
