ec06842d909c41a022f587a5d7c9f8a88aae52a6
zonin <business.kariukimartin@gmail.com>
2023-06-09 08:24:40 +0300

    render the app in a view

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import { View, StyleSheet } from 'react-native';
 import CurrentWeather from './src/components/CurrentWeather';
 const App = () => {
-  return <CurrentWeather />;
+  return (
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
 
 export default App;
