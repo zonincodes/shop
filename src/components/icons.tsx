2d3084bea1ee44fe9a83af18ebd21918d37b3c2c
zonin <business.kariukimartin@gmail.com>
2023-06-13 12:01:13 +0300

    restructure

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,1 +1,21 @@
 import React from "react";
+
+import {View, Text, StyleSheet} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+
+const IconText = ({name, color, text, style} : any)=> {
+
+    return (
+      <View style={styles.container}>
+        <Icon name={name} size={50} color={color} />
+        <Text style={style}>{text}</Text>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    }
+})
+export default IconText
